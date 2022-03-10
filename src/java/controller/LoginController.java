44e2d22cc83f330b5bba6e3585b8605a26b4f859
Windows 10 <hungpham307@gmail.com>
2022-03-10 20:41:22 +0700

    save user info to cookie

--- src/java/controller/LoginController.java
+++ src/java/controller/LoginController.java
@@ -21,34 +21,58 @@ import model.Account;
  */
 public class LoginController extends HttpServlet {
 
-   
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        Cookie[] cookies = request.getCookies();
+        String username = null;
+        String password = null;
+
+        for (Cookie cooky : cookies) {
+            if (cooky.getName().equals("username")) {
+                username = cooky.getValue();
+            }
+            if (cooky.getName().equals("password")) {
+                password = cooky.getValue();
+            }
+            if (username != null && password != null) {
+                break;
+            }
+        }
+
+         // if there's acocunt in cookie, don't need to authorize again
+        if (username != null && password != null) {
+            Account account = new AccountDAO().login(username, password);
+            if (account != null) { // valid cookie
+                request.getSession().setAttribute("account", account);
+                response.sendRedirect("campsites");
+                return;
+            }
+        }
+
         request.getRequestDispatcher("login.jsp").forward(request, response);
     }
 
-    
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
         boolean remember = request.getParameter("remember") != null;
-        
+
         // valid info => save to session, otherwise deny access
         Account account = new AccountDAO().login(username, password);
-        if (account!=null) {
+        if (account != null) {
             if (remember) {
                 Cookie uCookie = new Cookie("username", username);
-                uCookie.setMaxAge(60*60*24); // 1 day duration
+                uCookie.setMaxAge(60 * 60 * 24); // 1 day duration
                 Cookie pCookie = new Cookie("password", password);
-                pCookie.setMaxAge(60*60*24);
-                
+                pCookie.setMaxAge(60 * 60 * 24);
+
                 response.addCookie(uCookie);
                 response.addCookie(pCookie);
-            } 
-            
+            }
+
             request.getSession().setAttribute("account", account);
             response.sendRedirect("campsites");
         } else {
