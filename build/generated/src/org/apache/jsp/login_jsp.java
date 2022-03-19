c89eecff24456c1ba8f51badf756d3bbbceca2ae
Windows 10 <hungpham307@gmail.com>
2022-03-19 20:37:30 +0700

    restyling login page

--- build/generated/src/org/apache/jsp/login_jsp.java
+++ build/generated/src/org/apache/jsp/login_jsp.java
@@ -11,10 +11,31 @@ public final class login_jsp extends org.apache.jasper.runtime.HttpJspBase
 
   private static java.util.List<String> _jspx_dependants;
 
+  static {
+    _jspx_dependants = new java.util.ArrayList<String>(1);
+    _jspx_dependants.add("/components/navBar.jsp");
+  }
+
+  private org.apache.jasper.runtime.TagHandlerPool _jspx_tagPool_c_choose;
+  private org.apache.jasper.runtime.TagHandlerPool _jspx_tagPool_c_otherwise;
+  private org.apache.jasper.runtime.TagHandlerPool _jspx_tagPool_c_when_test;
+
   private org.glassfish.jsp.api.ResourceInjector _jspx_resourceInjector;
 
   public java.util.List<String> getDependants() {
     return _jspx_dependants;
+  }
+
+  public void _jspInit() {
+    _jspx_tagPool_c_choose = org.apache.jasper.runtime.TagHandlerPool.getTagHandlerPool(getServletConfig());
+    _jspx_tagPool_c_otherwise = org.apache.jasper.runtime.TagHandlerPool.getTagHandlerPool(getServletConfig());
+    _jspx_tagPool_c_when_test = org.apache.jasper.runtime.TagHandlerPool.getTagHandlerPool(getServletConfig());
+  }
+
+  public void _jspDestroy() {
+    _jspx_tagPool_c_choose.release();
+    _jspx_tagPool_c_otherwise.release();
+    _jspx_tagPool_c_when_test.release();
   }
 
   public void _jspService(HttpServletRequest request, HttpServletResponse response)
@@ -50,29 +71,89 @@ public final class login_jsp extends org.apache.jasper.runtime.HttpJspBase
       out.write("    <head>\n");
       out.write("        <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\n");
       out.write("        <link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css\" rel=\"stylesheet\" integrity=\"sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3\" crossorigin=\"anonymous\">\n");
-      out.write("        <title>JSP Page</title>\n");
+      out.write("        <link rel=\"shortcut icon\" type=\"image/png\" href=\"assets/images/camping.png\">\n");
+      out.write("        <link rel=\"stylesheet\" href=\"css/campsites.css\">\n");
+      out.write("        <title>Login</title>\n");
+      out.write("        ");
+      out.write("\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("<nav class=\"navbar navbar-expand-lg navbar-light bg-light fixed-top\">\n");
+      out.write("    <div class=\"container-fluid mx-3 \">\n");
+      out.write("        <a class=\"navbar-brand\" href=\"home\">Camping Paradise</a>\n");
+      out.write("        <button class=\"navbar-toggler\" type=\"button\" data-bs-toggle=\"collapse\" data-bs-target=\"#navbarNavDropdown\"\n");
+      out.write("                aria-controls=\"navbarNavDropdown\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">\n");
+      out.write("            <span class=\"navbar-toggler-icon\"></span>\n");
+      out.write("        </button>\n");
+      out.write("        <div class=\"collapse navbar-collapse  d-flex justify-content-between\" id=\"navbarNavDropdown\">\n");
+      out.write("            <ul class=\"navbar-nav\">\n");
+      out.write("                <li class=\"nav-item\">\n");
+      out.write("                    <a class=\"nav-link active\" aria-current=\"page\" href=\"home\">Home</a>\n");
+      out.write("                </li>\n");
+      out.write("                <li class=\"nav-item\">\n");
+      out.write("                    <a class=\"nav-link\" href=\"#\">Features</a>\n");
+      out.write("                </li>\n");
+      out.write("                <li class=\"nav-item\">\n");
+      out.write("                    <a class=\"nav-link\" href=\"campsites\">Camp Sites</a>\n");
+      out.write("                </li>\n");
+      out.write("            </ul>\n");
+      out.write("\n");
+      out.write("            <form action=\"search\" class=\"d-flex\">\n");
+      out.write("                <input class=\"form-control me-2\" type=\"search\" name=\"keyword\" placeholder=\"Search for campsite\">\n");
+      out.write("                <button class=\"btn btn-outline-success\" type=\"submit\">Search</button>\n");
+      out.write("            </form>\n");
+      out.write("\n");
+      out.write("            <div class=\"d-flex my-2\">\n");
+      out.write("                <a class=\"btn btn-outline-dark\" href=\"book\">\n");
+      out.write("                    <i class=\"bi-cart-fill me-1\"></i>\n");
+      out.write("                    Your Booking\n");
+      out.write("                    <span class=\"badge bg-dark text-white ms-1 rounded-pill\">");
+      out.write((java.lang.String) org.apache.jasper.runtime.PageContextImpl.evaluateExpression("${sessionScope.book.size()}", java.lang.String.class, (PageContext)_jspx_page_context, null));
+      out.write("</span>\n");
+      out.write("                </a>\n");
+      out.write("                ");
+      if (_jspx_meth_c_choose_0(_jspx_page_context))
+        return;
+      out.write("\n");
+      out.write("            </div>\n");
+      out.write("        </div>\n");
+      out.write("\n");
+      out.write("    </div>\n");
+      out.write("</nav>\n");
+      out.write("                \n");
+      out.write("                <style>\n");
+      out.write("                    .btn {\n");
+      out.write("                        transition: all 0.3s;\n");
+      out.write("                    }\n");
+      out.write("                </style>\n");
+      out.write("\n");
       out.write("    </head>\n");
       out.write("    <body>\n");
-      out.write("        <div style=\"width: 40%; margin: auto\" class=\"mt-5\">\n");
-      out.write("                <form action=\"login\" method=\"POST\">\n");
-      out.write("                    <div class=\"mb-3\">\n");
-      out.write("                        <label for=\"username\" class=\"form-label\">Username</label>\n");
-      out.write("                        <input type=\"text\" class=\"form-control\"  name=\"username\" >\n");
-      out.write("                    </div>\n");
-      out.write("                    <div class=\"mb-3\">\n");
-      out.write("                        <label for=\"password\" class=\"form-label\">Password</label>\n");
-      out.write("                        <input type=\"text\" class=\"form-control\" name=\"password\">\n");
-      out.write("                    </div>\n");
-      out.write("                    <div class=\"mb-3 form-check\">\n");
-      out.write("                        <input type=\"checkbox\" class=\"form-check-input\"  name=\"remember\">\n");
-      out.write("                        <label class=\"form-check-label\" >Remember me</label>\n");
-      out.write("                    </div>\n");
-      out.write("                    <h3 class=\"text-danger\">");
+      out.write("        <div class=\"login\">\n");
+      out.write("            <h1 class=\"mt-4\">Please log in first</h1>\n");
+      out.write("            <form action=\"login\" method=\"POST\">\n");
+      out.write("                <div class=\"mb-3\">\n");
+      out.write("                    <label for=\"username\" class=\"form-label\">Username</label>\n");
+      out.write("                    <input type=\"text\" class=\"form-control\"  name=\"username\" >\n");
+      out.write("                </div>\n");
+      out.write("                <div class=\"mb-3\">\n");
+      out.write("                    <label for=\"password\" class=\"form-label\">Password</label>\n");
+      out.write("                    <input type=\"password\" class=\"form-control\" name=\"password\">\n");
+      out.write("                </div>\n");
+      out.write("                <div class=\"mb-3 form-check\">\n");
+      out.write("                    <input type=\"checkbox\" class=\"form-check-input\"  name=\"remember\">\n");
+      out.write("                    <label class=\"form-check-label\" >Remember me</label>\n");
+      out.write("                </div>\n");
+      out.write("                <h3 class=\"text-danger\">");
       out.write((java.lang.String) org.apache.jasper.runtime.PageContextImpl.evaluateExpression("${error}", java.lang.String.class, (PageContext)_jspx_page_context, null));
       out.write("</h3>\n");
-      out.write("                    <button type=\"submit\" class=\"btn btn-primary\">Submit</button>\n");
-      out.write("                </form>\n");
+      out.write("                <button type=\"submit\" class=\"btn btn-primary\">Submit</button>\n");
+      out.write("            </form>\n");
+      out.write("                <a href=\"signup\" class=\"mt-3 text-center\">Don't have account? Sign up here!</a>\n");
       out.write("        </div>\n");
+      out.write("\n");
       out.write("        <script src=\"https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js\"></script>\n");
       out.write("    </body>\n");
       out.write("</html>\n");
@@ -87,5 +168,97 @@ public final class login_jsp extends org.apache.jasper.runtime.HttpJspBase
     } finally {
       _jspxFactory.releasePageContext(_jspx_page_context);
     }
+  }
+
+  private boolean _jspx_meth_c_choose_0(PageContext _jspx_page_context)
+          throws Throwable {
+    PageContext pageContext = _jspx_page_context;
+    JspWriter out = _jspx_page_context.getOut();
+    //  c:choose
+    org.apache.taglibs.standard.tag.common.core.ChooseTag _jspx_th_c_choose_0 = (org.apache.taglibs.standard.tag.common.core.ChooseTag) _jspx_tagPool_c_choose.get(org.apache.taglibs.standard.tag.common.core.ChooseTag.class);
+    _jspx_th_c_choose_0.setPageContext(_jspx_page_context);
+    _jspx_th_c_choose_0.setParent(null);
+    int _jspx_eval_c_choose_0 = _jspx_th_c_choose_0.doStartTag();
+    if (_jspx_eval_c_choose_0 != javax.servlet.jsp.tagext.Tag.SKIP_BODY) {
+      do {
+        out.write("\n");
+        out.write("                    ");
+        if (_jspx_meth_c_when_0((javax.servlet.jsp.tagext.JspTag) _jspx_th_c_choose_0, _jspx_page_context))
+          return true;
+        out.write("\n");
+        out.write("                    ");
+        if (_jspx_meth_c_otherwise_0((javax.servlet.jsp.tagext.JspTag) _jspx_th_c_choose_0, _jspx_page_context))
+          return true;
+        out.write("\n");
+        out.write("                ");
+        int evalDoAfterBody = _jspx_th_c_choose_0.doAfterBody();
+        if (evalDoAfterBody != javax.servlet.jsp.tagext.BodyTag.EVAL_BODY_AGAIN)
+          break;
+      } while (true);
+    }
+    if (_jspx_th_c_choose_0.doEndTag() == javax.servlet.jsp.tagext.Tag.SKIP_PAGE) {
+      _jspx_tagPool_c_choose.reuse(_jspx_th_c_choose_0);
+      return true;
+    }
+    _jspx_tagPool_c_choose.reuse(_jspx_th_c_choose_0);
+    return false;
+  }
+
+  private boolean _jspx_meth_c_when_0(javax.servlet.jsp.tagext.JspTag _jspx_th_c_choose_0, PageContext _jspx_page_context)
+          throws Throwable {
+    PageContext pageContext = _jspx_page_context;
+    JspWriter out = _jspx_page_context.getOut();
+    //  c:when
+    org.apache.taglibs.standard.tag.rt.core.WhenTag _jspx_th_c_when_0 = (org.apache.taglibs.standard.tag.rt.core.WhenTag) _jspx_tagPool_c_when_test.get(org.apache.taglibs.standard.tag.rt.core.WhenTag.class);
+    _jspx_th_c_when_0.setPageContext(_jspx_page_context);
+    _jspx_th_c_when_0.setParent((javax.servlet.jsp.tagext.Tag) _jspx_th_c_choose_0);
+    _jspx_th_c_when_0.setTest(((java.lang.Boolean) org.apache.jasper.runtime.PageContextImpl.evaluateExpression("${sessionScope.account != null}", java.lang.Boolean.class, (PageContext)_jspx_page_context, null)).booleanValue());
+    int _jspx_eval_c_when_0 = _jspx_th_c_when_0.doStartTag();
+    if (_jspx_eval_c_when_0 != javax.servlet.jsp.tagext.Tag.SKIP_BODY) {
+      do {
+        out.write("\n");
+        out.write("                        <button class=\"btn btn-outline-primary ms-lg-2\">");
+        out.write((java.lang.String) org.apache.jasper.runtime.PageContextImpl.evaluateExpression("${sessionScope.account.displayName}", java.lang.String.class, (PageContext)_jspx_page_context, null));
+        out.write("</button>\n");
+        out.write("                        <a href=\"logout\" class=\"btn btn-outline-primary ms-lg-2\">Logout</a>\n");
+        out.write("                    ");
+        int evalDoAfterBody = _jspx_th_c_when_0.doAfterBody();
+        if (evalDoAfterBody != javax.servlet.jsp.tagext.BodyTag.EVAL_BODY_AGAIN)
+          break;
+      } while (true);
+    }
+    if (_jspx_th_c_when_0.doEndTag() == javax.servlet.jsp.tagext.Tag.SKIP_PAGE) {
+      _jspx_tagPool_c_when_test.reuse(_jspx_th_c_when_0);
+      return true;
+    }
+    _jspx_tagPool_c_when_test.reuse(_jspx_th_c_when_0);
+    return false;
+  }
+
+  private boolean _jspx_meth_c_otherwise_0(javax.servlet.jsp.tagext.JspTag _jspx_th_c_choose_0, PageContext _jspx_page_context)
+          throws Throwable {
+    PageContext pageContext = _jspx_page_context;
+    JspWriter out = _jspx_page_context.getOut();
+    //  c:otherwise
+    org.apache.taglibs.standard.tag.common.core.OtherwiseTag _jspx_th_c_otherwise_0 = (org.apache.taglibs.standard.tag.common.core.OtherwiseTag) _jspx_tagPool_c_otherwise.get(org.apache.taglibs.standard.tag.common.core.OtherwiseTag.class);
+    _jspx_th_c_otherwise_0.setPageContext(_jspx_page_context);
+    _jspx_th_c_otherwise_0.setParent((javax.servlet.jsp.tagext.Tag) _jspx_th_c_choose_0);
+    int _jspx_eval_c_otherwise_0 = _jspx_th_c_otherwise_0.doStartTag();
+    if (_jspx_eval_c_otherwise_0 != javax.servlet.jsp.tagext.Tag.SKIP_BODY) {
+      do {
+        out.write("\n");
+        out.write("                        <a href=\"login\" class=\"btn btn-outline-primary ms-lg-2\">Login</a>\n");
+        out.write("                    ");
+        int evalDoAfterBody = _jspx_th_c_otherwise_0.doAfterBody();
+        if (evalDoAfterBody != javax.servlet.jsp.tagext.BodyTag.EVAL_BODY_AGAIN)
+          break;
+      } while (true);
+    }
+    if (_jspx_th_c_otherwise_0.doEndTag() == javax.servlet.jsp.tagext.Tag.SKIP_PAGE) {
+      _jspx_tagPool_c_otherwise.reuse(_jspx_th_c_otherwise_0);
+      return true;
+    }
+    _jspx_tagPool_c_otherwise.reuse(_jspx_th_c_otherwise_0);
+    return false;
   }
 }
--- build/web/login.jsp
+++ build/web/login.jsp
@@ -12,29 +12,32 @@
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
         <link rel="shortcut icon" type="image/png" href="assets/images/camping.png">
-        <%@include file="components/navBar.jsp" %>
+        <link rel="stylesheet" href="css/loginout.css">
         <title>Login</title>
+        <%@include file="components/navBar.jsp" %>
     </head>
-    <body style="padding-top: 10rem">
-        <div style="width: 40%; margin: auto" class="mt-5">
-                <form action="login" method="POST">
-                    <div class="mb-3">
-                        <label for="username" class="form-label">Username</label>
-                        <input type="text" class="form-control"  name="username" >
-                    </div>
-                    <div class="mb-3">
-                        <label for="password" class="form-label">Password</label>
-                        <input type="password" class="form-control" name="password">
-                    </div>
-                    <div class="mb-3 form-check">
-                        <input type="checkbox" class="form-check-input"  name="remember">
-                        <label class="form-check-label" >Remember me</label>
-                    </div>
-                    <h3 class="text-danger">${error}</h3>
-                    <button type="submit" class="btn btn-primary">Submit</button>
-                </form>
+    <body>
+        <div class="login">
+            <h1 class="mt-4">Please log in first</h1>
+            <form action="login" method="POST">
+                <div class="mb-3">
+                    <label for="username" class="form-label">Username</label>
+                    <input type="text" class="form-control"  name="username" >
+                </div>
+                <div class="mb-3">
+                    <label for="password" class="form-label">Password</label>
+                    <input type="password" class="form-control" name="password">
+                </div>
+                <div class="mb-3 form-check">
+                    <input type="checkbox" class="form-check-input"  name="remember">
+                    <label class="form-check-label" >Remember me</label>
+                </div>
+                <h3 class="text-danger">${error}</h3>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+                <a href="signup" class="text-center cta">Don't have account? Sign up here!</a>
         </div>
-                    
+
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     </body>
 </html>
